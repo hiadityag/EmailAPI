91147527525f0311cd96c6721d6286471af60eb2
Aditya Gupta <24256982+hiadityag@users.noreply.github.com>
2018-07-28 21:56:11 +0530

    Update Move.java

--- src/com/aditya/Move.java
+++ src/com/aditya/Move.java
@@ -9,15 +9,17 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.HttpSession;
-
-import com.serivces.MyData;
 import com.services.TotalData;
+
 @WebServlet("/Move")
 public class Move extends HttpServlet {
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		HttpSession hs=request.getSession();
 		hs.setAttribute("email",request.getParameter("email1"));
 		hs.setAttribute("password",request.getParameter("password"));
+		System.out.println(request.getParameter("email"));
+		System.out.println(request.getParameter("password"));
+		
 		TotalData td = new TotalData();
 		td.setEmailPass(request.getParameter("email"),request.getParameter("password"));
 		RequestDispatcher rd=request.getRequestDispatcher("Welcome.jsp");
